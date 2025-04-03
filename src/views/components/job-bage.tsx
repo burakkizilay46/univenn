7e3cd9e4787b1e6857b1c49f9d563b24290f8f5c
Burak Kızılay <burak.kizilay@westerops.com>
2025-04-04 02:59:41 +0300

    fix for build errors

--- src/views/components/job-bage.tsx
+++ src/views/components/job-bage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const JobBage = ({ title }: { title: string }) => {
   return (
     <div className="px-3 py-1 text-sm font-semibold text-[#6927DA] bg-[#F5F3FF] rounded-full border-[#C3B5FD] border-[1px] w-fit">
